232072c07368c1af3216fcd40f6c15969dc0a644
JustinSheng <justin.wang4@icloud.com>
2024-03-29 16:08:36 +0800

    Toggle Constraints

--- PackingList/ViewController.swift
+++ PackingList/ViewController.swift
@@ -44,6 +44,8 @@ final class ViewController: UIViewController {
 	
 	@IBOutlet private var menuHeightConstraint: NSLayoutConstraint!
 	@IBOutlet private var menuButtonTrailingConstriant: NSLayoutConstraint!
+	@IBOutlet private var titleCenterYClosedConstraint: NSLayoutConstraint!
+	@IBOutlet private var titleCenterYOpenConstraint: NSLayoutConstraint!
 
   //MARK:- Variables
   
@@ -72,19 +74,24 @@ private extension ViewController {
 		titleLabel.text = menuIsOpen ? "Select Item!" : "Packing List"
 		view.layoutIfNeeded()
 		
+		titleCenterYOpenConstraint.isActive = menuIsOpen
+		titleCenterYClosedConstraint.isActive = !menuIsOpen
+		
 		let constraints = titleLabel.superview!.constraints
 		constraints.first(where: { $0.firstItem === titleLabel && $0.firstAttribute == .centerX })?.constant = menuIsOpen ? -100 : 0
-		constraints.first(where: { $0.identifier == "Title Center Y" })!.isActive = false
-		let constraint = NSLayoutConstraint(item: titleLabel!,
-																				attribute: .centerY,
-																				relatedBy: .equal,
-																				toItem: titleLabel.superview,
-																				attribute: .centerY,
-																				multiplier: menuIsOpen ? 2 / 3 : 1,
-																				constant: 0)
-		constraint.identifier = "Title Center Y"
-		constraint.priority = .defaultHigh
-		constraint.isActive = true
+		
+		// Replaced by two constraints
+//		constraints.first(where: { $0.identifier == "Title Center Y" })!.isActive = false
+//		let constraint = NSLayoutConstraint(item: titleLabel!,
+//																				attribute: .centerY,
+//																				relatedBy: .equal,
+//																				toItem: titleLabel.superview,
+//																				attribute: .centerY,
+//																				multiplier: menuIsOpen ? 2 / 3 : 1,
+//																				constant: 0)
+//		constraint.identifier = "Title Center Y"
+//		constraint.priority = .defaultHigh
+//		constraint.isActive = true
 		
 		menuHeightConstraint.constant = menuIsOpen ? 200 : 80
 		menuButtonTrailingConstriant.constant = menuIsOpen ? 16 : 8
